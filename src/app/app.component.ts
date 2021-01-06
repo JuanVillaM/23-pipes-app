5d9c9c460c2f8b97345565c3fbb7d08e8b8dd7c6
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-06 15:43:12 -0400

    Folder app/ added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,43 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'spotify';
+  
+  PI: number = Math.PI;
+  
+  salary: number = 1234.5;
+  
+  percentege: number = 0.234;
+  
+  name: string = 'Juan Villa';
+ 
+  wrongName: string = 'jUaN vILLa MEndOzA';
+  
+  array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  date: Date = new Date();
+
+  activate: boolean = true;
+
+  urlVideo: string = 'https://www.youtube.com/embed/pyg-DYm7b0A';
+
+  language: string = 'es';
+  
+  hero = {
+    name: 'Logan',
+    heroName: 'Wolverine',
+    age: 500,
+    direction: {
+      avenue: 'Fortnite',
+      house: 69
+    }
+  }
+
+  promiseValue = new Promise<string>( ( resolve ) => {
+    setTimeout(() => {
+      resolve('The data arrived!');
+    }, 4500);
+  })
 }
